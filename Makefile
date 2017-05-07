b9725ce5e7602908507e13864d46f38f852efd20
Tobias Neumerkel <github@unique2.org>
2017-05-07 14:17:06 +0200

    Add makefile to build w/o Arduino IDE
    Since the Arduino IDE has connection problems it is more convinient
    to be able to build and flash the board without the GUI.
    
    This commit provides a makefile based build toolchain to the project.
    Therefore also the build environment is resturctured to separate sourcefiles
    from object and buildsystem files.

---
+++ Makefile
@@ -0,0 +1,8 @@
+BOARD_TAG   = nano
+BOARD_SUB   = atmega328
+ARDUINO_DIR = lnk-arduino
+MONITOR_PORT = com3
+OBJDIR = bin/obj
+USB_TYPE = USB_SERIAL
+
+include build/Arduino.mk
--- midifuchs.ino
+++ midifuchs.ino
@@ -1,6 +1,5 @@
-#include <EEPROM.h>
 #include "hitdetection.h"
-#include "hitdetection.c"
+//#include "hitdetection.c"
 #include "calculateVelocity.h"
 
 const int midiCommandNoteOn = 144;
@@ -11,16 +10,22 @@ const int MIDDLE = 80; // Wird bei Midi als "mittlere" Velocity genutzt.
 const int OFF = 0;
 const int note = noteC4;
 
+static void noteOn(int midiCommand, int midiNote, int midiVelocity);
+extern int hitDetection_initialize();
+extern int hitDetection_process(unsigned int i_analogValue);
+
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  hitDetection_initialize();
+  int a = hitDetection_initialize();
+  (void)a;
 
   /* WICHTIG:
     Auf dem Leonardo-Board muss unbedingt "Serial1" genutzt werden, da "Serial" den USB-Port anspricht.
     Das Leonardo-Board besitzt nur einen Chip, auf dem beide Schnittstellen verwaltet werden
   */
-  Serial1.begin(31250); // "31250" ist die Baudrate für MIDI!
+  //Serial.begin(31250); // "31250" ist die Baudrate für MIDI!
   
   // Für das Logging!!
   Serial.begin(9600);
@@ -30,12 +35,14 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  unsigned int sensorValue = (unsigned int)analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
 
-  if(hitDetection_process(sensorValue)) {
+  Serial.println("sensorValue");
+  Serial.println(sensorValue);
+  if(1 == hitDetection_process(sensorValue)) {
     noteOn(midiCommandNoteOn, note, calculateVelocity(sensorValue));        
     noteOn(midiCommandNoteOn, note, OFF);
     Serial.println("Sendet MIDI-Note: ");
@@ -47,8 +54,8 @@ void loop() {
 
 void noteOn(int midiCommand, int midiNote, int midiVelocity) {
   //midiVelocity = (midiVelocity < MAX) ? midiVelocity : MAX; // Wenn Velocity über 127, dann auf 127 setzen, damit MIDI-Maxwert nicht überstiegen wird!
-  Serial1.write(midiCommand);
-  Serial1.write(midiNote);
-  Serial1.write(midiVelocity);
+  Serial.write(midiCommand);
+  Serial.write(midiNote);
+  Serial.write(midiVelocity);
 }
 
